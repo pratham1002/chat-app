a3372710fe01c27733885d19b10c3cf0798e914f
Pratham Gupta <f20190051@pilani.bits-pilani.ac.in>
2020-03-11 22:33:08 +0530

    Counter built

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,13 @@
-io();
+const socket = io();
+
+socket.on("countUpdated", (count) => {
+    console.log("Count has been updated ", count);
+    document.getElementById("count").innerHTML = count;
+});
+
+document.querySelector("#increment").addEventListener("click", () => {
+    console.log("Clicked");
+
+    socket.emit("increment");
+    
+});
